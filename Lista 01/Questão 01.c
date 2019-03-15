a8e69b1f1774034a7ea93cd2d8f3623d6886fb2c
Érica Sousa <ericasousa@alu.ufc.br>
2019-03-15 20:23:44 -0300

    Adicionando informações de pacientes e imprimindo suas informações.

--- Lista 01/Questão 01.c
+++ Lista 01/Questão 01.c
@@ -42,33 +42,70 @@ int main(){
 	Prontuario prontuario_02;
 	Paciente paciente;
 	
-	for(i = 0; i <=2; i++){
-		printf("\nInformações do paciente\nNome : ");
-		fgets(prontuario_01.nome, sizeof(prontuario_01.nome), stdin);
-		
-		printf("Endereço : ");
-		fgets(prontuario_01.endereco, sizeof(prontuario_01), stdin);
-		
-		printf("Telefone : ");
-		fgets(prontuario_01.telefone, sizeof(prontuario_01), stdin);
-		
-		printf("Altura : ");
-		scanf("%f", &prontuario_01.altura);
-		fflush(stdin);
-		
-		printf("Data de nascimento : ");
-		fgets(prontuario_01.data_nasc, sizeof(prontuario_01), stdin);
-		
-		printf("Peso : ");
-		scanf("%.2f", &prontuario_01.peso);
-		fflush(stdin);
-	}
 	
-	for(i=0; i <=3; i++){
-		printf("Prontuario: \n%s\n%s\n%s\n%.2f\n%s\n%.2f", prontuario_01.nome, prontuario_01.endereco, prontuario_01.telefone, prontuario_01.altura, prontuario_01.data_nasc, prontuario_01.peso);
-	}
+	printf("\nProntuário 01\nNome : ");
+	fgets(prontuario_01.nome, sizeof(prontuario_01.nome), stdin);
+		
+	printf("Endereço : ");
+	fgets(prontuario_01.endereco, sizeof(prontuario_01), stdin);
+		
+	printf("Telefone : ");
+	fgets(prontuario_01.telefone, sizeof(prontuario_01), stdin);
+		
+	printf("Altura : ");
+	scanf("%f", &prontuario_01.altura);
+	fflush(stdin);
+		
+	printf("Data de nascimento : ");
+	fgets(prontuario_01.data_nasc, sizeof(prontuario_01), stdin);
+		
+	printf("Peso : ");
+	scanf("%f", &prontuario_01.peso);
+	fflush(stdin);
 	
+	printf("\nProntuário 02\nNome : ");
+	fgets(prontuario_02.nome, sizeof(prontuario_02.nome), stdin);
+		
+	printf("Endereço : ");
+	fgets(prontuario_02.endereco, sizeof(prontuario_02), stdin);
+		
+	printf("Telefone : ");
+	fgets(prontuario_02.telefone, sizeof(prontuario_02), stdin);
+		
+	printf("Altura : ");
+	scanf("%f", &prontuario_02.altura);
+	fflush(stdin);
+		
+	printf("Data de nascimento : ");
+	fgets(prontuario_02.data_nasc, sizeof(prontuario_02), stdin);
+		
+	printf("Peso : ");
+	scanf("%f", &prontuario_02.peso);
+	fflush(stdin);
 	
+	printf("\nInformações do paciente\nNome : ");
+	fgets(paciente.nome, sizeof(prontuario_01.nome), stdin);
+		
+	printf("Endereço : ");
+	fgets(paciente.endereco, sizeof(prontuario_01), stdin);
+		
+	printf("Telefone : ");
+	fgets(paciente.telefone, sizeof(prontuario_01), stdin);
+		
+	printf("Altura : ");
+	scanf("%f", &paciente.altura);
+	fflush(stdin);
+		
+	printf("Data de nascimento : ");
+	fgets(paciente.data_nasc, sizeof(paciente), stdin);
+		
+	printf("Peso : ");
+	scanf("%f", &paciente.peso);
+	fflush(stdin);
+	
+	printf("\nProntuario 01:\nNome: %sEndereço: %sTelefone: %sAltura: %.2f\nData Nascimento: %sPeso: %.2f\n", prontuario_01.nome, prontuario_01.endereco, prontuario_01.telefone, prontuario_01.altura, prontuario_01.data_nasc, prontuario_01.peso);
+	printf("\nProntuario 02:\nNome: %sEndereço: %sTelefone: %sAltura: %.2f\nData Nascimento: %sPeso: %.2f\n", prontuario_02.nome, prontuario_02.endereco, prontuario_02.telefone, prontuario_02.altura, prontuario_02.data_nasc, prontuario_02.peso);
+	printf("\nPaciente:\nNome: %sEndereço: %sTelefone: %sAltura: %.2f\nData Nascimento: %sPeso: %.2f", paciente.nome, paciente.endereco, paciente.telefone, paciente.altura, paciente.data_nasc, paciente.peso);
 	
 	return 0;
 }
